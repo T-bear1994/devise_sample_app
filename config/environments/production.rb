02b88be2e74ed6138c9d427f72eabdbc14c36cfe
Takahiro Iizuka <agdjgt@gmail.com>
2022-06-17 07:35:06 +0900

    Modified production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { :host => 'https://aqueous-sierra-31982.herokuapp.com/' }
-config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :letter_opener_web
 config.action_mailer.smtp_settings = {
   :enable_starttls_auto => true,
   :address => "smtp.sendgrid.net",
