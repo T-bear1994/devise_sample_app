bfdbca59d113aafde03a5709ed576b2df380f52f
Takahiro Iizuka <agdjgt@gmail.com>
2022-06-17 07:19:53 +0900

    Modified mailer config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'blogs#index'
   resources :blogs
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
